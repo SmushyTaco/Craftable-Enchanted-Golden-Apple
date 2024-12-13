a636c7e95bbf173915912c93dd6698ac5cb58ce3
Nikan Radan <github.com@alias.nikanradan.com>
2024-12-13 00:52:39 -0800

    1.21.4

--- Craftable Enchanted Golden Apple/settings.gradle.kts
+++ Craftable Enchanted Golden Apple/settings.gradle.kts
@@ -5,5 +5,10 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-    plugins { id("fabric-loom").version(settings.extra["loom_version"] as String) }
+    plugins {
+        id("fabric-loom").version(settings.extra["loom_version"] as String)
+        id("com.modrinth.minotaur").version(settings.extra["minotaur_version"] as String)
+        id("net.darkhax.curseforgegradle").version(settings.extra["curseforge_gradle_version"] as String)
+        id("co.uzzu.dotenv.gradle").version(settings.extra["dotenv_version"] as String)
+    }
 }
